722481798c9965a716693f1f6ab728f8c6018cc5
Mohammad Rheza Zamani <56064595+rhezazz@users.noreply.github.com>
2022-04-04 21:05:33 +0700

    Delete VES BA directory

--- VES BA/misfit_VES.m
+++
@@ -1,7 +0,0 @@
-function [misfit] = misfit_VES(rho_obs, rho_cal)
-    ls = length(rho_obs);
-    for j = 1 : ls
-        m(j) = ((rho_obs(j) - rho_cal(j)))^2;
-    end
-    misfit = sqrt((1/ls)*sum(m));
-end
